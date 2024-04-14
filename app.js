f8cb52d07e46375c4e483694010fb7e07ed76075
michaelamando1 <michaelamando25@gmail.com>
2024-04-14 22:19:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+/* Three words used to define a variable
+var, let, const
+*/
+
+//Declare a Variable(Var)
+// var name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// //Initialising Variables
+// var greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+
+// /*Variables can include letters, numbers, 
+// _ , $ */
+// // Can not start with a number
+
+// //Multi word vars
+// var firstname = 'John'; // Camel case
+// var first_name = 'Sarah'; // Underscore method
+// var FirstName = 'Tom'; //Pascal Case
+
+// //LET 
+// let name;
+// name = 'John Doe';
+// console.log(name);
+// name = 'Steve Smith';
+// console.log(name);
+
+// CONST
+// const name = 'John';
+// console.log(name);
+
+//Can not reassign
+// name = 'Sarah';
+
+//Have to assign a value
+//const greeting;
+
+const person = {
+    name: 'John',
+    age: 30
+}
+
+person.name = 'Sarah'
+
+// console.log(person);
+
+const numbers = [1, 2, 3, 4, 5];
+numbers.push(6);
+
+console.log(numbers);
